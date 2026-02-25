a8f181ba77f0b54b013f7b9e114576808308cb57
oliveirasilvaana2604 <oliveira.silva.ana2604@escola.pr.gov.br>
2026-02-25 08:35:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const botao = document.getElementById("botao");
+const texto = document.getElementById("texto");
+const imagem = document.getElementById("imagem");
+
+botao.addEventListener("click", ()=>{
+    fetch("https://pokeapi.co/api/v2/pokemon/12") 
+    .then(resposta => resposta.json()) 
+    .then(dados => {
+        texto.textContent = dados.name;
+        imagem.src = dados.sprites.front_default;
+
+    });
+});
